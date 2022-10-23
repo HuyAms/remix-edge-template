15601a175f349256fb75c48a9d4c79ae1b9b9476
Huy Trinh <dinhhuyams@gmail.com>
2022-10-23 17:46:41 +0300

    feat: loader from node

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,20 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+
+export const loader: LoaderFunction = () => {
+  return {
+    data: 'HELLO DENO'
+  }
+}
+
 export default function Index() {
+
+  const {data} = useLoaderData()
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>{data}</h1>
       <ul>
         <li>
           <a
